7e4794365f6742fd82e0f244541518b8546a3422
Antoine Fabri <antoine.fabri@gmail.com>
2021-09-25 11:34:35 +0200

    document file focused condition helpers

--- R/current_file-condition-helpers.R
+++ R/current_file-condition-helpers.R
@@ -1,11 +1,12 @@
+#' File Focused Condition Helpers
 #' @export
-#' @rdname condition-helpers
+#' @rdname file-condition-helpers
 current_file_is_r_script <- function() {
   toupper(tools::file_ext(current_path())) == "R"
 }
 
 #' @export
-#' @rdname condition-helpers
+#' @rdname file-condition-helpers
 current_file_is_rmd <- function() {
   toupper(tools::file_ext(current_path())) == "RMD"
 }
