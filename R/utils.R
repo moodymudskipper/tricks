3e6bf8ac5fef6bccf195a3b315b2c404dab101cb
Antoine Fabri <antoine.fabri@gmail.com>
2021-09-25 11:20:38 +0200

    export 'fails' function

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,15 @@
-fails <- function(expr) {
+
+#' Test if a Call Fails
+#'
+#' This is mainly used internally and should be rarely needed but can be useful
+#' for some custom trick conditions. Use this with caution! The tested call is run
+#' so you don't want it to have potential side effects or be slow!
+#'
+#' @param call A call
+#' @export
+fails <- function(call) {
   eval.parent(substitute(
-    inherits(try(expr, silent = TRUE), "try-error")
+    inherits(try(call, silent = TRUE), "try-error")
   ))
 }
 
