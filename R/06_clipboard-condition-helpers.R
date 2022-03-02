2bda06b48e9074bbff90b5c64c97640482dd0bea
Antoine Fabri <antoine.fabri@gmail.com>
2022-03-02 16:25:59 +0100

    implement clipboard_text_matches()

--- R/06_clipboard-condition-helpers.R
+++ R/06_clipboard-condition-helpers.R
@@ -18,3 +18,13 @@ clipboard_is_parsable <- function() {
 clipboard_text <- function() {
   clipr::read_clip()
 }
+
+#' @export
+#'
+#' @rdname clipboard-condition-helpers
+clip_board_text_matches <- function(pattern, n_min = 1L, n_max = Inf, ...) {
+  clipboard_contains_text() && {
+    sum_ <- sum(regexpr(pattern, clipboard_text(), ...))
+    sum_ >= n_min && sum_ <= n_max
+  }
+}
